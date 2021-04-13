860f781eac144e6112a7bbf844f206bbae8d1b9a
cr542 <christian.reisenauer@gmx.at>
2021-04-13 11:49:31 +0200

    Worked out model

--- src/list/SortedLinkedList.java
+++ src/list/SortedLinkedList.java
@@ -1,7 +1,63 @@
 package list;
 
-public class SortedLinkedList {
+public class SortedLinkedList<T extends Comparable<T>> implements SortedList<T> {
+
+    private int size;
+    private Node head;
+    private Node tail;
+
+    @Override
+    public void insert(T element) {
+        Node node = new Node(element);
+        if(head == null) {
+            head = node;
+            increment();
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+            increment();
+        }
+    }
+
+    @Override
+    public boolean delete(int index) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public void increment() {
+        size++;
+    }
+
+    public void decrement() {
+        size--;
+    }
 
     private class Node {
+        private final T data;
+        private Node next;
+        private Node previous;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + data + "]";
+        }
+
     }
 }
--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Animal {
+public class Animal implements Comparable<Animal> {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String noise() {
+        return "Määäääh";
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        return 0;
+    }
 }
